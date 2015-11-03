9f8e4b2d73a49176c8376cad5eb147047ab82324
Stefano Peluchetti <phd.st.p@gmail.com>
2015-11-03 18:29:58 +0000

    Prepare for v1.0.2.

--- __meta.lua
+++ __meta.lua
@@ -1,10 +1,11 @@
 return {
-  version = "1.0.1",
+  name = "time",
+  version = "1.0.2",
   require = {
     luajit = "2.0",
     xsys   = "1.0",
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
   homepage = "http://scilua.org/time.html",
-  description = "TIME - Dates and Periods in Lua",
+  description = "dates and periods in Lua",
 }
